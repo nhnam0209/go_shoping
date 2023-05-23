d0088e950d400bbae723b76a50043669e09be04a
Kiet_Tran <kiet.tran@dirox.net>
2023-05-23 12:36:01 +0700

    Update

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,10 @@
 import './assets/main.css'
-
+import { VueScreenSizeMixin } from 'vue-screen-size';
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
-
 const app = createApp(App)
-
 app.use(createPinia())
-
+app.use(VueScreenSizeMixin)
 app.mount('#app')
+
